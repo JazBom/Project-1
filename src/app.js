044092a709ba954c8e3ace24ed42a01a003b5353
Jessica Bommer <xininu@gmail.com>
2020-12-08 20:21:06 +1100

    Disabling buttons and better text and CSS with placeholder images

--- src/app.js
+++ src/app.js
@@ -73,30 +73,47 @@ $("#call-api-btn1").on("click", () => {
     $('#call-api-btn2').removeClass('disabled');
     // modern art attacks
     modernArt.attackEnemy(classicArt);
+    console.log(classicArt);
     // write out gameplay....
     if(!modernArt.dead && !classicArt.dead){
         // gameplay if both still alive
-        console.log(classicArt);// object - display values under buttons?
-        $("#divArtBattle").prepend(`<h2 id="modernArtQuote">"Inhale colour!!! Die young!!!"</h2>`);
-        $("#modernArtQuote").append(`<p>${modernArt.name} slathers toxic cobalt violet on his enemy.</p>`); // replaces this with random sentence - build random sentence generator
-        $("#modernArtQuote").append(`<p>${classicArt.name} loses ${modernArt.attackpoint} year${modernArt.attackpoint>1?'s':''} of his life...  Only ${classicArt.hitpoints} good painting years left!</p>`); // fix plural
-        $("#modernArtQuote").append(`<p>That young upstart ${modernArt.name} is still alive and waiting for another chance... watch out ${classicArt.name}.</p>`);
+        // object - display values under buttons?
+        const quoteModernArt = [
+            `"Inhale colour!!! Die young!!!"`, 
+            `"If no mistake you have made, losing you are..."`,
+            `"Draw a circle... then imagine it's a head... or whatever you f&^%ing want it to be."`
+        ];
+        const sentence1A = [
+            `slathers toxic cobalt violet on his enemy.`,
+            `sells a finger painting for $1.5 million.`,
+            `doesn't use brown or black... and it looks amazing!!`
+        ];
+        const sentence1B = [
+            `is still drawing those perfect heads.`, 
+            `is respected by other artists, and will not retire!!`,
+            `takes abuse only from Kings and popes.`
+        ];
+        const random1 = Math.floor(Math.random() * quoteModernArt.length);
+        $("#divArtBattle").prepend(`<h1 id="modernArtQuote">${quoteModernArt[random1]}</h1>`);
+        $("#modernArtQuote").append(`<h2>${modernArt.name} ${sentence1A[random1]}</h2>`); // replaces this with random sentence - build random sentence generator
+        $("#modernArtQuote").append(`<p>${classicArt.name} loses ${modernArt.attackpoint} year${modernArt.attackpoint>1?'s':''} of his life...  Only ${classicArt.hitpoints} good painting years left.</p>`); // fix plural
+        $("#modernArtQuote").append(`<p>But ${classicArt.name} ${sentence1B[random1]}</p>`);
         console.log(classicArt);
     } else if(modernArt.dead && !classicArt.dead) {
         // console.log(`Picasso is the winner!!! Game over Raffaello.`)
         $("#divArtBattle").prepend(`<h2 "modernArtWinner">Picasso is the winner!!! Game over Raffaello.</h2>`);
-        $("#modernArtWinner").append(`<p>${modernArt.name} has ${modernArt.hitpoints} year${modernArt.hitpoints>1?'s':''} left..</p>`); 
-        $("#modernArtWinner").append(`<p>${classicArt.name} has ${classicArt.hitpoints} year${classicArt.hitpoints>1?'s':''} left.. </p>`);
-    } else if(!classicArt.dead && modernArt.dead){
+        $("#modernArtWinner").prepend(`<p>${modernArt.name} has no years left..</p>`); 
+        $("#modernArtWinner").prepend(`<p>${classicArt.name} has ${classicArt.hitpoints} year${classicArt.hitpoints>1?'s':''} left.. </p>`);
+    } else if(classicArt.dead && !modernArt.dead){
         // console.log(`Raffaello is the winner!!! Game over Picasso.`);
         $("#divArtBattle").prepend(`<h2 id="classicArtWinner">Raffaello is the winner!!! Game over Picasso.</h2>`);
-        $("#classicArtWinner").append(`<p>${modernArt.name} has ${modernArt.hitpoints} year${modernArt.hitpoints>1?'s':''} left..</p>`); 
-        $("#classicArtWinner").append(`<p>${classicArt.name} has ${classicArt.hitpoints} year${classicArt.hitpoints>1?'s':''} left.. </p>`);
+        $("#classicArtWinner").prepend(`<p>${modernArt.name} has ${modernArt.hitpoints} year${modernArt.hitpoints>1?'s':''} left.. </p>`);
+        $("#classicArtWinner").prepend(`<p>${classicArt.name} has no years left..</p>`); 
     } else if(classicArt.dead && modernArt.dead){
         // console.log(`Game over. No winners...art is dead...`);
         $("#divArtBattle").prepend(`<h2 id="gameOver">Game over. No winners...art is dead....</h2>`);
-        $("#gameOver").append(`<p>${modernArt.name} has ${modernArt.hitpoints} year${modernArt.hitpoints>1?'s':''} left..</p>`); 
-        $("#gameOver").append(`<p>${classicArt.name} has ${classicArt.hitpoints} year${classicArt.hitpoints>1?'s':''} left.. </p>`);
+        $("#gameOver").prepend(`<p>${modernArt.name} has no years left..</p>`); 
+        $("#gameOver").prepend(`<p> ... ${classicArt.name} also has no years left.. </p>`);
     } else {
         $("#divArtBattle").prepend(`<h2>Oops...look like there's paint on our face.</h2>`);
     };
@@ -108,7 +125,7 @@ $("#call-api-btn1").on("click", () => {
             const i = Math.floor(Math.random() * 31); // change '31' depending on how many images in array
             const dataModernArt = data1[i]// change [i] to [random number 1-500]
             const id1 = dataModernArt.id; 
-            const width = 300;
+            const width = 400;
             const height = 200;
             const author1 = dataModernArt.author;
             const url1 = `https://picsum.photos/id/${id1}/${width}/${height}`;
@@ -118,13 +135,13 @@ $("#call-api-btn1").on("click", () => {
             console.log(url1);
     
             // Creating elements to add those responses
-            const image1 = `<p id="imageP1"><img src="${url1}"></img></p>`;
+            const image1 = `<p id="imageP1"><img class="image-cell-2" src="${url1}"></img></p>`;
             const authorName1 = `<p>${author1}</p>`; 
             // const imageUrl = `<a id="3" href="${url}" target="_blank">Go to image</a>`;
             
             //Appending the elements on the DOM
             $("#divArtBattle").prepend(image1);
-            $("#imageP1").append(authorName1);
+            $("#imageP1").prepend(authorName1);
             // $("#col-2").append(imageUrl);
         }
     );
@@ -143,29 +160,49 @@ $("#call-api-btn2").on("click", () => {
     $('#call-api-btn1').removeClass('disabled');
     // classic art attacks
     classicArt.attackEnemy(modernArt);
+    console.log(modernArt);
     // gameplay
     if(!modernArt.dead && !classicArt.dead){
-        console.log(modernArt);// object - display values under buttons?
-        $("#divArtBattle").prepend(`<h2 id="classicArtQuote">"Eat my classic slow, painful lead poisoning for the sake of your art!!!"</h2>`);
-        $("#classicArtQuote").append(`<p>${classicArt.name} squeezes lead white in his enemy's face.</p>`); // replaces this with random sentence - build random sentence generator
-        $("#classicArtQuote").append(`<p>${modernArt.name} loses ${classicArt.attackpoint} year${classicArt.attackpoint>1?'s':''}  of his life...  Only ${modernArt.hitpoints} good painting years left!</p>`); // fix plural
-        $("#classicArtQuote").append(`<p>${classicArt.name} is still young and beautiful... watch out ${modernArt.name}.</p>`);
+        // object - display values under buttons?
+        const quoteModernArt = [
+            `"Eat my slow, painful lead poisoning for the sake of your art!!!"`, 
+            `"Paint or paint not, there is no try ..."`,
+            `"Draw a circle...then draw the rest of the f$%*ing head!"`
+        ];
+        const sentence2A = [
+            `squeezes lead white in his enemy's face...`,
+            `rejects ${modernArt.name}'s work from the Grand Salon.`,
+            `teaches this young upstart a lesson.`
+        ];
+        const sentence2B = [
+            `is still young and beautiful... watch out.`, 
+            `eats pain(t) for breakfast and is coming for you...`,
+            `is still in the studio... the student will soon become the master.`
+        ];
+        const random2 = Math.floor(Math.random() * quoteModernArt.length);
+        console.log(quoteModernArt[random2]);
+
+        $("#divArtBattle").prepend(`<h1 id="classicArtQuote">${quoteModernArt[random2]}</h1>`);
+        $("#classicArtQuote").append(`<h2>${classicArt.name} ${sentence2A[random2]}.</h2>`);
+        $("#classicArtQuote").append(`<p>${modernArt.name} loses ${classicArt.attackpoint} year${classicArt.attackpoint>1?'s':''}  of his life...  Only ${modernArt.hitpoints} good painting years left.</p>`);  // fix plural
+        $("#classicArtQuote").append(`<p>But ${modernArt.name} ${sentence2B[random2]}.</p>`);
         console.log(modernArt);
+
     } else if (!modernArt.dead && classicArt.dead){
         // console.log(`<p>Picasso is the winner!!! Game over Raffaello.</p>`)
         $("#divArtBattle").prepend(`<h2 "modernArtWinner">Picasso is the winner!!! Game over Raffaello.</h2>`);
-        $("#modernArtWinner").append(`<p>${modernArt.name} has ${modernArt.hitpoints} year${modernArt.hitpoints>1?'s':''} left..</p>`); 
-        $("#modernArtWinner").append(`<p>${classicArt.name} has ${classicArt.hitpoints} year${classicArt.hitpoints>1?'s':''} left.. </p>`);
+        $("#modernArtWinner").prepend(`<p>${modernArt.name} has ${modernArt.hitpoints} year${modernArt.hitpoints>1?'s':''} left..</p>`); 
+        $("#modernArtWinner").prepend(`<p>${classicArt.name} has no years left.. </p>`);
     } else if(!classicArt.dead && modernArt.dead){
         // console.log(`<p>Raffaello is the winner!!! Game over Picasso.</p>`);
         $("#divArtBattle").prepend(`<h2 id="classicArtWinner">Raffaello is the winner!!! Game over Picasso.</h2>`);
-        $("#classicArtWinner").append(`<p>${modernArt.name} has ${modernArt.hitpoints} year${modernArt.hitpoints>1?'s':''} left..</p>`); 
+        $("#classicArtWinner").append(`<p>${modernArt.name} has no years left..</p>`); 
         $("#classicArtWinner").append(`<p>${classicArt.name} has ${classicArt.hitpoints} year${classicArt.hitpoints>1?'s':''} left.. </p>`);
     } else if(classicArt.dead && modernArt.dead){
         // console.log(`Game over. No winners...art is dead...`);
         $("#divArtBattle").prepend(`<h2 id="gameOver">Game over. No winners...art is dead....</h2>`);
-        $("#gameOver").append(`<p>${modernArt.name} has ${modernArt.hitpoints} year${modernArt.hitpoints>1?'s':''} left..</p>`); 
-        $("#gameOver").append(`<p>${classicArt.name} has ${classicArt.hitpoints} year${classicArt.hitpoints>1?'s':''} left.. </p>`);
+        $("#gameOver").append(`<p>${modernArt.name} has no years left..</p>`); 
+        $("#gameOver").append(`<p>...${classicArt.name} also has no years left... </p>`);
     } else {
         $("#divArtBattle").prepend(`<h2>Oops...look like there's paint on our face.</h2>`);
     };
@@ -177,10 +214,10 @@ $("#call-api-btn2").on("click", () => {
                 console.log(data2);
                 // Extract info
                 // have to put in an if() statement for not returning results where 'undefined' (e.g. no url)
-                    const i = Math.floor(Math.random() * 31);  // change '31' depending on how many images in array
-                    const dataClassicArt = data2[i]
-                    const id2 = dataClassicArt.id; 
-                    const width = 300;
+                    const i = Math.floor(Math.random() * 30);  // change '31' depending on how many images in array
+                    const dataClassicArt = data2[i];
+                    const id2 = dataClassicArt.id; // why does this keep throwing an error?
+                    const width = 400;
                     const height = 200;
                     const author2 = dataClassicArt.author;
                     const url2 = `https://picsum.photos/id/${id2}/${width}/${height}`;
@@ -191,13 +228,13 @@ $("#call-api-btn2").on("click", () => {
             
                     // Creating elements to add those responses
                     
-                    const image2 = `<p id="imageP2"><img src="${url2}"></img></p>`;
+                    const image2 = `<p id="imageP2"><img class="image-cell-2" src="${url2}"></img></p>`;
                     const authorName2 = `<p>${author2}</p>`; 
                     // const imageUrl = `<a id="3" href="${url}" target="_blank">Go to image</a>`;
                     
                     //Appending the elements on the DOM
                     $("#divArtBattle").prepend(image2);
-                    $("#imageP2").append(authorName2);
+                    $("#imageP2").prepend(authorName2);
                     // $("#col-2").append(imageUrl);
                 }
             );
